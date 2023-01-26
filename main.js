481e8b39fef22e1c8e2d126e137a94f39011c972
BluCat <blucatnight@gmail.com>
2023-01-26 09:47:33 -0800

    trying to get the cloning to work

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+var clones = 0;
+const dartid = "dart";
+
+
+function show(object) {
+    object.style.display = "block";
+}
+function hide(object) {
+    object.style.display = "none";
+}
+
+function onloadfunction() {
+    //hide(document.getElementById("dart"))
+}
 
 function getmousepos() {
-    console.log(event.clientX)
-    console.log(event.clientY)
-    $('#dart').animate({
-
-    },400)
+    if (clones <= 3 ) {
+        clones += 1
+        console.log(event.clientX)
+        console.log(event.clientY)
+        console.log(clones)
+        var clone = $("#dart").clone()
+        clone.css('left', event.clientX-17 + "px")
+        clone.css('top', event.clientY-17 + "px")
+        $("#dart").css('left', event.clientX-17 + "px")
+        $("#dart").css('top', event.clientY-17 + "px")
+    }
 }
