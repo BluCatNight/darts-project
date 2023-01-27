db5315cec4e483722134c8c09b414a733ed85581
BluCat <blucatnight@gmail.com>
2023-01-27 08:53:40 -0800

    some unfinished code,

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 var clones = 0;
 const dartid = "dart";
 var clonelist = new Array();
+var x = []
+var y = []
+
+//center
+//427 x
+//270 y
+
+function check() {
+    //unfinished
+    var num = 0;
+    var final = 0;
+    var count = 0;
+    for(z in y) {
+        count += 1
+        var lastpoint = [x[z],y[z]]
+        num = Math.sqrt(lastpoint[0] + x[z] **2 + lastpoint[1] + y[z]**2)
+        final = final + num
+    }
+    final = final / count
+    console.log(final)
+    console.log(count)
+}
+
 
 function onloadfunction() {
     $("#dart").hide()
@@ -11,6 +34,9 @@ function onresetfunction() {
         clonelist[cln].remove(); //Need help i cant get this to work.
     }
     clones = 0;
+    x = []
+    y = []
+    clonelist = new Array();
 }
 
 
@@ -22,11 +48,13 @@ function getmousepos() {
         console.log(event.clientY)
         console.log(clones)
         var clone = $("#dart").clone().appendTo(".target1");
-        clone.css('left', event.clientX-17 + "px")
-        clone.css('top', event.clientY-17 + "px")
+        clone.css('left', event.clientX-6 + "px")
+        clone.css('top', event.clientY-6 + "px")
         clone.show(50)
         clonelist.push(clone)
-        $("#dart").css('left', event.clientX-17 + "px")
-        $("#dart").css('top', event.clientY-17 + "px")
+        y.push(event.clientY-6)
+        x.push(event.clientX-6)
+        $("#dart").css('left', event.clientX-6 + "px")
+        $("#dart").css('top', event.clientY-6 + "px")
     }
 }
